b69d85522c6d6796cb55170ce57570dbc467c451
agyanim <agyanimconsultancy@gmail.com>
2023-05-08 16:43:14 +0000

    category page made dynamic

--- app/page.jsx
+++ app/page.jsx
@@ -6,29 +6,25 @@ import Link from "next/link";
 const Home = async () => {
   const { events_categories } = await import("../data/data.json");
 
-  const renderEvent = events_categories.map((events) => {
-    return (
-      <section>
-        <Link href={`/category/${events.id}`} key={events.id}>
-          <Image
-            src={events.image}
-            width={200}
-            height={100}
-            alt="Event image"
-          />
-          <h2>{events.title}</h2>
-          <p>{events.description}</p>
-        </Link>
-      </section>
-    );
-  });
-
   return (
     <>
       <main className="flex flex-col gap-5 w-[80vw] m-auto">
         <h1>Events</h1>
 
-        {renderEvent}
+        {events_categories.map((events) => (
+          <section>
+            <Link href={`/category/${events.id}`} key={events.id}>
+              <Image
+                src={events.image}
+                width={200}
+                height={100}
+                alt="Event image"
+              />
+              <h2>{events.title}</h2>
+              <p>{events.description}</p>
+            </Link>
+          </section>
+        ))}
       </main>
     </>
   );

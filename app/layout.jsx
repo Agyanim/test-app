9594a0d6d1db309ca5a77e51cca7ee1364a38b39
agyanim <agyanimconsultancy@gmail.com>
2023-05-10 09:55:01 +0000

    google font removed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
 import Nav from '@/components/nav'
 import '../style/globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'My First Next Application',
@@ -12,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Nav/>
         {children}</body>
     </html>

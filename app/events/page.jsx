5254c796a2dc5af1d0a302125657f1b698b67b7a
agyanim <agyanimconsultancy@gmail.com>
2023-05-10 15:23:21 +0000

    event page modified

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,60 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const EventsPage = () => {
+const EventsPage =async () => {
+  const { events_categories } = await import("../../data/data.json");
+
   return (
     <>
       <main className="flex flex-col gap-5 w-[80vw] m-auto">
+        <h1>Events</h1>
 
-        <h1>Evants</h1>
-        <h2></h2>
-        <section>
-          {/* <a href="/category/london"> */}
-            <img src="/images/category/ev-cat-london.jpeg" alt="lodon city" />
-            <h2>Events In London</h2>
-            <p className=" text-red-950">
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered alteration in some form, by
-              injected humour, or randomised words which don't look even
-              slightly believable. If you are going to use a passage of Lorem
-              Ipsum, you need to be sure there isn't anything embarrassing
-              hidden in the middle of text.
-            </p>
-          {/* </a> */}
-        </section>
-        <section>
-          <a href="">
-            <img
-              src="/images/category/ev-cat-sanFrancisco.jpg"
-              alt="lodon city"
-            />
-            <h2>Events In San-Francisco</h2>
-            <p>
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered alteration in some form, by
-              injected humour, or randomised words which don't look even
-              slightly believable. If you are going to use a passage of Lorem
-              Ipsum, you need to be sure there isn't anything embarrassing
-              hidden in the middle of text.
-            </p>
-          </a>
-        </section>
-        <section>
-          <a href="">
-            <img
-              src="/images/category/ev-cat-barcelona.jpeg"
-              alt="lodon city"
-            />
-            <h2>Events In Barcelona</h2>
-            <p>
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered alteration in some form, by
-              injected humour, or randomised words which don't look even
-              slightly believable. If you are going to use a passage of Lorem
-              Ipsum, you need to be sure there isn't anything embarrassing
-              hidden in the middle of text.
-            </p>
-          </a>
-        </section>
+        {events_categories.map((events) => (
+          <section>
+          <Link href={`/category/${events.id}`} key={events.id}>
+              <Image
+                src={events.image}
+                width={400}
+                height={200}
+                alt="Event image"
+              />
+              <h2>{events.title}</h2>
+              <p>{events.description}</p>
+            </Link>
+          </section>
+        ))}
       </main>
     </>
   );
